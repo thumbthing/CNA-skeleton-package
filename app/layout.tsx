be241f553354e44ef8ad9ac2275ac138a361d213
thumbthing <ilscc0902@gmail.com>
2023-09-15 23:11:07 +0900

    Chore: styled-components delay bug 해결을 위한 registry 파일 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import StyledComponentsRegistry from './lib/registry';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,7 +14,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <StyledComponentsRegistry>
+        {children}
+        </StyledComponentsRegistry>
+      </body>
     </html>
   );
 }
